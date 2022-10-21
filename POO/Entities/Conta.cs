afae46634b0d4190481c7f03003022774b621c90
Thiago S Adriano <tadriano.net@gmail.com>
2022-10-21 10:03:02 -0300

    primeira aula OOP

---
+++ POO/Entities/Conta.cs
@@ -0,0 +1,55 @@
+﻿namespace POO.Entities
+{
+    public class Conta
+    {
+        public Conta(string titular)
+        {
+            Titular = titular;
+            NumeroDaConta = Guid.NewGuid();
+            Saldo = 0;
+        }
+
+        #region [Propriedades]
+        public string Titular { get; set; }
+
+        private Guid NumeroDaConta { get; set; }
+
+        public int Saldo { get; private set; }
+        #endregion
+
+        #region [Metodos]
+        public Guid RetornarNumeroConta()
+        {
+            return NumeroDaConta;
+        }
+
+        public void Depositar(int valor)
+        {
+            Saldo += valor;
+            // return $"Valor {valor} depositado com sucesso!";
+        }
+
+        public void Sacar(int valor)
+        {
+            if (Saldo > valor)
+            {
+                Saldo -= valor;
+            }        
+        }
+
+        public void Transferir(Conta contaDestino, int valor)
+        {
+
+            if (Saldo > valor)
+            {
+                Saldo -= valor;
+                contaDestino.Depositar(valor);
+            }        
+
+          
+        }
+        #endregion
+
+
+    }
+}
---
+++ POO/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using POO.Entities;
+
+var conta = new Conta("Sirius Black");
+//conta.Depositar(1000);
+conta.Sacar(1000);
+
+
+Console.WriteLine($"A conta titular: {conta.Titular} numero: {conta.RetornarNumeroConta()} foi criada corretamente com o saldo: {conta.Saldo}");
+
+
+var contaB = new Conta("Harry Potter");
+
+
+Console.WriteLine($"A conta titular: {contaB.Titular} numero: {contaB.RetornarNumeroConta()} foi criada corretamente com o saldo: {contaB.Saldo}");
+
+
+conta.Transferir(contaB, 600);
+
+Console.WriteLine($"Depois da transferencia Sirius: {conta.Saldo}");
+Console.WriteLine($"Depois da transferencia Harry: {contaB.Saldo}");
+
+
+
+Console.ReadLine();
